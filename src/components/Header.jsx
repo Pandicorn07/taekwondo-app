7a63d902a2b1defc906c6a5a98a64cffce70908c
JuniorCoder <thomas08samuel@gmail.com>
2022-02-20 20:57:54 +0100

    header-update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,6 @@ const Header = () => {
     return (
         <div className="header">
             <h1 id="heading">TWD</h1>
-
-            <div className="burger-menu">
-                <div className="line01 line"></div>
-                <div className="line02 line"></div>
-                <div className="line03 line"></div>
-            </div>
         </div>
     );
 };
